f4a3e970ea3e01c716753aa919afb8ef53314182
DanielleSpyra <dspyra@colgate.edu>
2024-12-03 18:21:47 -0500

    Update SleepyHebbians.m

--- SleepyHebbians.m
+++ SleepyHebbians.m
@@ -4,3 +4,17 @@ clear all
 clc
 
 % this is a test
+
+% for synaptic scaling (Sullivan & Sa, 2008)
+beta_n = 0.01; %time constant
+y = 0.01; % γ is a constant between zero and 1 that controls the time course of the accumulation.
+C_targ = 10; % in Hz. Target value to compare with chemical concentration
+alpha = 1; % learning rate
+num_updates = 100; % number of updates between sleep cycles
+num_iter = 200; % number of iterations per sleep cycle
+total_iter = 45000; % total number of iterations
+
+% Ct =γzt +(1−γ)Ct−1
+
+
+
